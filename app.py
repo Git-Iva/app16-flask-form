5cd7a83017525802a3968c824c05a9d45a8e111d
iva <ivanela.lazarova@gmail.com>
2024-01-08 14:25:38 +0800

    Create the database with Flask's SQLAlchemy extension

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# Specify the parameters of the database
+app.config["SECRET_KEY"] = "myapplication123"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+db = SQLAlchemy(app)
+
+
+# Create the database by using a database model
+class Form(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    date = db.Column(db.Date)
+    occupation = db.Column(db.String(80))
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method =="POST":
+    if request.method == "POST":
         first_name = request.form["first_name"]
         last_name = request.form["last_name"]
         email = request.form["email"]
@@ -13,4 +28,8 @@ def index():
     return render_template("index.html")
 
 
-app.run(debug=True, port=5001)
+if __name__ == "__main__":
+    with app.app_context():
+        # Calls Form class; Database not overwritten once created
+        db.create_all()
+        app.run(debug=True, port=5001)

a616c90ac57ed7bcc616b971718b4d51da59ebf2
iva <ivanela.lazarova@gmail.com>
2024-01-08 16:08:26 +0800

    Implement a subbmission notification message

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -35,6 +35,7 @@ def index():
                     date=date_object, occupation=occupation)
         db.session.add(form)
         db.session.commit()
+        flash("Your form was submitted successfully!", "success")
 
 
     return render_template("index.html")
